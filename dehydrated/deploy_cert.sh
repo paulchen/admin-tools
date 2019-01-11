5abe872aa67be5daae8e35a9a42ee38027e7efd5
Paul Staroch <paulchen@rueckgr.at>
2019-01-11 20:10:50 +0100

    improved error handling

--- dehydrated/deploy_cert.sh
+++ dehydrated/deploy_cert.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+
+ERROR=0
+
+fail() {
+	ERROR=1
+	echo "Fail!"
+}
+
+
 if [ "$1" == "" ]; then
 	echo Missing parameter
 	exit 1
@@ -20,57 +29,57 @@ source "$CONFIGFILE"
 
 for domain in "${IRC_CERTS[@]}"; do
 	if [ "$domain" == "all" ] || [ "$domain" == "$1" ]; then
-		rsync -av /etc/dehydrated/certs/irc.rueckgr.at/ /etc/inspircd/ssl/
-		chown -R irc:irc /etc/inspircd/ssl/
+		rsync -av /etc/dehydrated/certs/irc.rueckgr.at/ /etc/inspircd/ssl/ || fail
+		chown -R irc:irc /etc/inspircd/ssl/ || fail
 		echo Reloading IRCd configuration due to renewal of certificate for domain $1 ...
-		"$FILEPATH/rehash_irc.sh"
+		"$FILEPATH/rehash_irc.sh" || fail
 	fi
 done
 
 for domain in "${POSTFIX_CERTS[@]}"; do
 	if [ "$domain" == "all" ] || [ "$domain" == "$1" ]; then
 		echo Restarting Postfix due to renewal of certificate for domain $1 ...
-		systemctl restart postfix.service
+		systemctl restart postfix.service || fail
 	fi
 done
 
 for domain in "${DOVECOT_CERTS[@]}"; do
 	if [ "$domain" == "all" ] || [ "$domain" == "$1" ]; then
 		echo Restarting Dovecot due to renewal of certificate for domain $1 ...
-		systemctl restart dovecot.service
+		systemctl restart dovecot.service || fail
 	fi
 done
 
 for domain in "${APACHE_CERTS[@]}"; do
 	if [ "$domain" == "all" ] || [ "$domain" == "$1" ]; then
 		echo Reloading Apache due to renewal of certificate for domain $1 ...
-		systemctl force-reload apache2.service
+		systemctl force-reload apache2.service || fail
 	fi
 done
 
 for domain in "${LIGHTTPD_CERTS[@]}"; do
 	if [ "$domain" == "all" ] || [ "$domain" == "$1" ]; then
 		echo Reloading LigHTTPd due to renewal of certificate for domain $1 ...
-		cat "/etc/dehydrated/certs/$domain/privkey.pem" "/etc/dehydrated/certs/$domain/cert.pem" > "/etc/dehydrated/certs/$domain/lighttpd.pem"
-		systemctl force-reload lighttpd.service
+		cat "/etc/dehydrated/certs/$domain/privkey.pem" "/etc/dehydrated/certs/$domain/cert.pem" > "/etc/dehydrated/certs/$domain/lighttpd.pem" || fail
+		systemctl force-reload lighttpd.service || fail
 	fi
 done
 
 for domain in "${XMPP_CERTS[@]}"; do
 	if [ "$domain" == "all" ] || [ "$domain" == "$1" ]; then
 		echo Replacing XMPP certificate...
-		cat "/etc/dehydrated/certs/$domain/fullchain.pem" "/etc/dehydrated/certs/$domain/privkey.pem" > "/opt/ejabberd/etc/ejabberd/${domain}.pem"
-		chown ejabberd:ejabberd "/opt/ejabberd/etc/ejabberd/${domain}.pem"
+		cat "/etc/dehydrated/certs/$domain/fullchain.pem" "/etc/dehydrated/certs/$domain/privkey.pem" > "/opt/ejabberd/etc/ejabberd/${domain}.pem" || fail
+		chown ejabberd:ejabberd "/opt/ejabberd/etc/ejabberd/${domain}.pem" || fail
 	fi
 done
 
 for domain in "${NAGIOS_CERTS[@]}"; do
         if [ "$domain" == "all" ] || [ "$domain" == "$1" ]; then
                 echo Restarting nagios-nrpe-server due to renewal of certificate for domain $1 ...
-                mkdir -p "/etc/nagios/ssl/$domain"
-                cp "/etc/dehydrated/certs/$domain/privkey.pem" "/etc/dehydrated/certs/$domain/fullchain.pem" "/etc/nagios/ssl/$domain/"
-                chown -R nagios:nagios /etc/nagios/ssl
-                systemctl restart nagios-nrpe-server.service
+                mkdir -p "/etc/nagios/ssl/$domain" || fail
+                cp "/etc/dehydrated/certs/$domain/privkey.pem" "/etc/dehydrated/certs/$domain/fullchain.pem" "/etc/nagios/ssl/$domain/" || fail
+                chown -R nagios:nagios /etc/nagios/ssl || fail
+                systemctl restart nagios-nrpe-server.service || fail
         fi
 done
 
