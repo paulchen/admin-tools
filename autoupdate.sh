ac7aea5119f7659effc056fa736175e0ed090462
Paul Staroch <paulchen@rueckgr.at>
2019-06-04 19:37:16 +0200

    automatically update phpMyAdmin

--- autoupdate.sh
+++ autoupdate.sh
@@ -13,6 +13,10 @@ git_pull() {
 	echo ""
 }
 
+DIRECTORY=`dirname "$0"`
+cd "$DIRECTORY"
+DIRECTORY=`pwd`
+
 git_pull /var/www/default/web/rss >> /var/log/autoupdate.log 2>&1
 git_pull /opt/munin-contrib >> /var/log/autoupdate.log 2>&1 
 git_pull /var/www/mail/roundcube/plugins/carddav/ >> /var/log/autoupdate.log 2>&1 
@@ -21,4 +25,12 @@ git_pull /opt/nextcloud-munin-py >> /var/log/autoupdate.log 2>&1
 git_pull /var/www/default/web/rss-bridge >> /var/log/autoupdate.log 2>&1
 git_pull /opt/admin-tools >> /var/log/autoupdate.log 2>&1
 git_pull /opt/icinga-plugins >> /var/log/autoupdate.log 2>&1
+
+INSTALLED=`/opt/icinga-plugins/update-checker/applications/phpmyadmin/update_installed.sh`
+AVAILABLE=`/opt/icinga-plugins/update-checker/applications/phpmyadmin/update_available.sh`
+
+if [ "$INSTALLED" != "$AVAILABLE" ]; then
+	cd "$DIRECTORY"
+	./update-pma/update.sh --auto
+fi
 
