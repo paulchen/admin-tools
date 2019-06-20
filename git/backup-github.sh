04f36dd5a77e747351b77f2ba39cf169ff0c28e5
Paul Staroch <paulchen@rueckgr.at>
2019-06-20 20:21:59 +0200

    added backup scripts for GitHub and GitLab repositories

---
+++ git/backup-github.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+DIRECTORY=`dirname "$0"`
+cd "$DIRECTORY"
+
+. config.github
+
+LINK="https://api.github.com/user/repos?per_page=10"
+
+ERROR=0
+while true; do
+	REPOS=`curl -H "Authorization: token $TOKEN" "$LINK" 2>/dev/null |grep full_name|sed -e 's/^.*: "//;s/".*$//'`
+	for REPO in $REPOS; do
+		echo "Repository $REPO..."
+
+		cd "$BASEDIR"
+		mkdir -p "$REPO"
+		cd "$REPO"
+
+		if [ -e .git ]; then
+			git pull || ERROR=1
+		else
+			cd ..
+			git clone "git@github.com:$REPO" || ERROR=1
+		fi
+	done
+
+	IFS_OLD="$IFS"
+	export IFS=','
+
+	LINKS=$(curl --head -H "Authorization: token $TOKEN" "$LINK" 2>/dev/null|grep '^Link:'|sed -e 's/^Link: //g')
+	FOUND=0
+	for CANDIDATE_LINK in $LINKS; do
+		if [ "`echo $CANDIDATE_LINK|grep -c next`" -ne "0" ]; then
+			FOUND=1
+			LINK="`echo $CANDIDATE_LINK|sed -e 's/^.*<//;s/>.*//'`"	
+			break
+		fi
+	done
+	export IFS="$IFS_OLD"
+
+	if [ "$FOUND" -eq "0" ]; then
+		break
+	fi
+
+done
+
+if [ "$ERROR" -eq "0" ]; then
+	touch "$STATUSFILE"
+fi
+
+exit $ERROR
+
---
+++ git/backup-gitlab.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+DIRECTORY=`dirname "$0"`
+cd "$DIRECTORY"
+
+. config.gitlab
+
+LINK="https://gitlab.com/api/v4/projects?private_token=$TOKEN&membership=yes&per_page=1"
+
+ERROR=0
+while true; do
+	REPOS=`curl "$LINK" 2>/dev/null|python -m json.tool|grep path_with_namespace|sed -e 's/^.*: "//;s/".*$//'`
+	for REPO in $REPOS; do
+		echo "Repository $REPO..."
+
+		cd "$BASEDIR"
+		mkdir -p "$REPO"
+		cd "$REPO"
+
+		if [ -e .git ]; then
+			git pull || ERROR=1
+		else
+			cd ..
+			git clone "git@gitlab.com:$REPO" || ERROR=1
+		fi
+	done
+
+	IFS_OLD="$IFS"
+	export IFS=','
+
+	LINKS=$(curl --head "$LINK" 2>/dev/null|grep '^Link:'|sed -e 's/^Link: //g')
+	FOUND=0
+	for CANDIDATE_LINK in $LINKS; do
+		if [ "`echo $CANDIDATE_LINK|grep -c next`" -ne "0" ]; then
+			FOUND=1
+			LINK="`echo $CANDIDATE_LINK|sed -e 's/^.*<//;s/>.*//'`"	
+			break
+		fi
+	done
+	export IFS="$IFS_OLD"
+
+	if [ "$FOUND" -eq "0" ]; then
+		break
+	fi
+done
+
+if [ "$ERROR" -eq "0" ]; then
+	touch "$STATUSFILE"
+fi
+
+exit $ERROR
+
