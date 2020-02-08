1cc0c66c6c1507e5126a4d36159c63359326d121
Paul Staroch <paulchen@rueckgr.at>
2020-02-08 14:24:25 +0100

    add check whether update is already necessary

--- autoupdate.sh
+++ autoupdate.sh
@@ -18,18 +18,24 @@ git_pull() {
 }
 
 DIRECTORY=`dirname "$0"`
+
 cd "$DIRECTORY"
 DIRECTORY=`pwd`
+STATUS_FILE="$DIRECTORY/autoupdate.status"
 
-git_pull /var/www/default/web/rss >> /var/log/autoupdate.log 2>&1
-git_pull /opt/munin-contrib >> /var/log/autoupdate.log 2>&1 
-git_pull /var/www/mail/roundcube/plugins/carddav/ >> /var/log/autoupdate.log 2>&1 
-git_pull /var/www/default/web/rss/plugins.local/tumblr_gdpr >> /var/log/autoupdate.log 2>&1
-git_pull /opt/nextcloud-munin-py >> /var/log/autoupdate.log 2>&1
-git_pull /var/www/default/web/rss-bridge >> /var/log/autoupdate.log 2>&1
-git_pull /opt/admin-tools >> /var/log/autoupdate.log 2>&1
-git_pull /opt/icinga-plugins >> /var/log/autoupdate.log 2>&1
-git_pull /home/paulchen/ipwe "paulchen:paulchen" >> /var/log/autoupdate.log 2>&1
+/opt/icinga-plugins/check_fileage.py -f "$STATUS_FILE" -w 150 -c 160 >> /dev/null 2>&1 && exit
+
+ERROR=0
+
+git_pull /var/www/default/web/rss >> /var/log/autoupdate.log 2>&1 || ERROR=1
+git_pull /opt/munin-contrib >> /var/log/autoupdate.log 2>&1 || ERROR=1
+git_pull /var/www/mail/roundcube/plugins/carddav/ >> /var/log/autoupdate.log 2>&1 || ERROR=1
+git_pull /var/www/default/web/rss/plugins.local/tumblr_gdpr >> /var/log/autoupdate.log 2>&1 || ERROR=1
+git_pull /opt/nextcloud-munin-py >> /var/log/autoupdate.log 2>&1 || ERROR=1
+git_pull /var/www/default/web/rss-bridge >> /var/log/autoupdate.log 2>&1 || ERROR=1
+git_pull /opt/admin-tools >> /var/log/autoupdate.log 2>&1 || ERROR=1
+git_pull /opt/icinga-plugins >> /var/log/autoupdate.log 2>&1 || ERROR=1
+git_pull /home/paulchen/ipwe "paulchen:paulchen" >> /var/log/autoupdate.log 2>&1 || ERROR=1
 
 /opt/icinga-plugins/update-checker/applications/phpmyadmin/update_installed.sh > /dev/null 2>&1 || exit 0
 
@@ -38,6 +44,13 @@ AVAILABLE=`/opt/icinga-plugins/update-checker/applications/phpmyadmin/update_ava
 
 if [ "$INSTALLED" != "$AVAILABLE" ]; then
 	cd "$DIRECTORY"
-	./update-pma/update.sh --auto
+	./update-pma/update.sh --auto || ERROR=1
 fi
+
+if [ "$ERROR" -eq "0" ]; then
+	pwd
+	touch "$STATUS_FILE"
+fi
+
+exit "$ERROR"
 
