3d9b3c4a702aae0c161571ea9f63158d623626c0
Paul Staroch <paulchen@rueckgr.at>
2020-03-04 22:48:09 +0100

    add /opt/check_ssl_cert

--- autoupdate.sh
+++ autoupdate.sh
@@ -30,6 +30,7 @@ git_pull /var/www/default/web/rss-bridge >> /var/log/autoupdate.log 2>&1
 git_pull /opt/admin-tools >> /var/log/autoupdate.log 2>&1
 git_pull /opt/icinga-plugins >> /var/log/autoupdate.log 2>&1
 git_pull /home/paulchen/ipwe "paulchen:paulchen" >> /var/log/autoupdate.log 2>&1
+git_pull /opt/check_ssl_cert/ >> /var/log/autoupdate.log 2>&1
 
 /opt/icinga-plugins/update-checker/applications/phpmyadmin/update_installed.sh > /dev/null 2>&1 || exit 0
 
