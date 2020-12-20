39541f38eec3546a2283546f21c2a1c8a99df2d9
Paul Staroch <paulchen@rueckgr.at>
2020-12-20 01:40:48 +0100

    automatically try to switch to correct user; introduce nextcloud.conf

--- update-nextcloud.sh
+++ update-nextcloud.sh
@@ -1,41 +1,70 @@
 #!/bin/bash
 
+CONFIG_FILE=`dirname $0`/nextcloud.conf
+
+if [ ! -e "$CONFIG_FILE" ]; then
+	echo "Configuration file $CONFIG_FILE not found"
+	exit 3
+fi
+
+. "$CONFIG_FILE"
+
+if [ ! -f "$DIRECTORY/occ" ]; then
+	echo "Invalid NextCloud directory: $DIRECTORY"
+	exit 3
+fi
+
+USER=`stat -c '%U' "$DIRECTORY/occ"`
+CURRENT_USER=`whoami`
+
+if [ "$USER" != "$CURRENT_USER" ]; then
+	if [ "$CURRENT_USER" != "root" ]; then
+		echo "Please run this script either as user $USER or as root"
+		exit 3
+	fi
+
+	sudo -u "$USER" "$0"
+	exit
+fi
+
+
 # inspired by from https://sysadms.de/2020/08/nextcloud-updates-automatisieren/
 
 ERROR=0
 
 echo "Downloading and installing update"
 
-php /var/www/cloud/updater/updater.phar --no-interaction || ERROR=1
+php "$DIRECTORY/updater/updater.phar" --no-interaction || ERROR=1
 
 if [ "$ERROR" -eq "0" ]; then
 	echo "Executing necessary upgrades"
-	php /var/www/cloud/occ upgrade --no-interaction || ERROR=1
+	php "$DIRECTORY/occ" upgrade --no-interaction || ERROR=1
 fi
 
 if [ "$ERROR" -eq "0" ]; then
 	echo "Adding missing columns"
-	php /var/www/cloud/occ db:add-missing-columns --no-interaction || ERROR=1
+	php "$DIRECTORY/occ" db:add-missing-columns --no-interaction || ERROR=1
 fi
 
 if [ "$ERROR" -eq "0" ]; then
 	echo "Adding missing columns"
-	php /var/www/cloud/occ db:add-missing-primary-keys --no-interaction || ERROR=1
+	php "$DIRECTORY/occ" db:add-missing-primary-keys --no-interaction || ERROR=1
 fi
 
 if [ "$ERROR" -eq "0" ]; then
 	echo "Adding missing indices"
-	php /var/www/cloud/occ db:add-missing-indices --no-interaction || ERROR=1
+	php "$DIRECTORY/occ" db:add-missing-indices --no-interaction || ERROR=1
 fi
 
 if [ "$ERROR" -eq "0" ]; then
 	echo "Perform any available app updates"
-	php /var/www/cloud/occ app:update --all --no-interaction || ERROR=1
+	php "$DIRECTORY/occ" app:update --all --no-interaction || ERROR=1
 fi
 
 if [ "$ERROR" -eq "0" ]; then
 	echo "Update successfully completed"
 else
 	echo "Some error occurred during update"
+	exit 1
 fi
 
