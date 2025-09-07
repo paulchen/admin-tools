7f611222882d802ffec3f6b3f3a30b6cfd10a10f
Paul Staroch <paulchen@rueckgr.at>
2025-09-07 23:23:40 +0200

    provide environment variable MONGO_IMAGE_DIGEST to kotlin-rocket-bot

--- docker-compose/kotlin-rocket-bot/start.sh
+++ docker-compose/kotlin-rocket-bot/start.sh
@@ -3,5 +3,8 @@
 export DOCKER_VERSION="$(docker -v)"
 export LINUX_VERSION="$(uname -a)"
 
+MONGO_IMAGE="$(docker inspect --format='{{.Config.Image}}' mongo)"
+export MONGO_IMAGE_DIGEST="$(docker inspect --format='{{index .RepoDigests 0}}' $MONGO_IMAGE | sed -e 's/^.*://')"
+
 docker compose -f /opt/admin-tools/docker-compose/kotlin-rocket-bot/docker-compose.yml up -d --remove-orphans
 
