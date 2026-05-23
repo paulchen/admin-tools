778d7aebca91c168d20446d3d49ddb3ba34b659e
Paul Staroch <paulchen@rueckgr.at>
2026-05-23 13:45:26 +0200

    ignore AH01095 error code

--- apache_error_log/apache_error_log.sh
+++ apache_error_log/apache_error_log.sh
@@ -63,6 +63,7 @@ find /var/www /var/log \( -name error.log -or -name error.log.1 -or -name contai
 	grep -v 'AH10244' | # invalid URI path
 	grep -v 'AH02608' | # read request body failed
 	grep -v 'AH01084' | # pass request body failed
+	grep -v 'AH01095' | # prefetch request body failed
 	grep -v 'AH01097' | # pass request body failed
 	grep -v 'DisplayAction->execute' | # RSS bridge
 	grep -v 'Empty module and/or action after parsing the URL' | # Symfony framework
