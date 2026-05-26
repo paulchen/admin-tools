6fddf936d255bf8d49e9b12f389f238bbd1cc3c9
Paul Staroch <paulchen@rueckgr.at>
2026-05-26 22:18:35 +0200

    ignore AH02032 error code

--- apache_error_log/apache_error_log.sh
+++ apache_error_log/apache_error_log.sh
@@ -65,6 +65,7 @@ find /var/www /var/log \( -name error.log -or -name error.log.1 -or -name contai
 	grep -v 'AH01084' | # pass request body failed
 	grep -v 'AH01095' | # prefetch request body failed
 	grep -v 'AH01097' | # pass request body failed
+	grep -v 'AH02032' | # no compatible SSL setup
 	grep -v 'DisplayAction->execute' | # RSS bridge
 	grep -v 'Empty module and/or action after parsing the URL' | # Symfony framework
 	grep -v 'This request has been forwarded to a 404 error page' | # Symfony framework
